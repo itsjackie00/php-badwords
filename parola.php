5ec568a3911729e24d2f228b0410bc440ada16cc
itsjackie00 <jackiedanso33@gmail.com>
2024-04-29 19:23:13 +0200

    end

---
+++ parola.php
@@ -0,0 +1,27 @@
+<?php
+
+$parola = $_POST['parola'];
+$frase = $_POST['frase'];
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>PHP Badwords</title>
+</head>
+
+<body>
+    <header class="m-2">
+        <h3><?php echo $frase ?>, <?php echo $parola ?></h3>
+    </header>
+    <main>
+        <p><?php echo str_replace($parola, '***', $frase) ?></p>
+    </main>
+</body>
+
+</html>
