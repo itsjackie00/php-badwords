615d4ebbcbb713394801f86206f03e52937c2f77
itsjackie00 <jackiedanso33@gmail.com>
2024-04-29 18:31:04 +0200

    added boostrap link, h4

--- index.php
+++ index.php
@@ -1,16 +1,24 @@
+<?php
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>PHP Badwords</title>
 </head>
 <body>
     <header>
-        <h1>
-            ciao
-        </h1>
+        <h1>PHP Badwords</h1>
+        <h4>Questo è il paragrafo lungo che conterra la tua parola inserita in input!</h4>
+        <h4>Inserisci la tua parola ...</h4>
     </header>
+    <main>
+        
+    </main>
     
 </body>
 </html>
